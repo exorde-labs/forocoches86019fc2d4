a6eea40ec1632e2a9e0a1568d7053582ea983a1c
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-02-29 00:24:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="forocoches86019fc2d4",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "exorde_data",
